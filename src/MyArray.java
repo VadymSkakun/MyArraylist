73911be653667fdeb8748ccb8bfa1c22921a9409
VadymSkakun <jamal_q@ukr.net>
2019-06-22 23:21:13 +0300

    Initial commit

---
+++ src/MyArray.java
@@ -0,0 +1,56 @@
+import java.util.Iterator;
+
+public class MyArray<E> implements Operations<E> {
+    private E[] values;
+
+    public MyArray() {
+        values = (E[]) new Object[0];
+    }
+
+    @Override
+    public boolean add(E e) {
+        try {
+            E[] temp = values;
+            values = (E[]) new Object[temp.length + 1];
+            System.arraycopy(temp, 0, values, 0, temp.length);
+            values[values.length - 1] = e;
+            return true;
+        } catch (ClassCastException ex1) {
+            ex1.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public void delete(int index) {
+        try {
+            E[] temp = values;
+            values = (E[]) new Object[temp.length - 1];
+            System.arraycopy(temp, 0, values, 0, index);
+            int amountElementAfterIndex = temp.length - index - 1;
+            System.arraycopy(temp, index + 1, values, index, amountElementAfterIndex);
+        } catch (ClassCastException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public E get(int index) {
+        return values[index];
+    }
+
+    @Override
+    public int size() {
+        return values.length;
+    }
+
+    @Override
+    public void update(int index, E e) {
+        values[index] = e;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new ArrayIterator<>(values);
+    }
+}
